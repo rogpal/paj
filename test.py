5987d02f9d813c585fe1b3cb067d50c134eb04e1
rogpal <rogurt@hotmail.com>
2015-10-01 16:11:21 +0200

    Changed structure.

--- test.py
+++ test.py
@@ -5,26 +5,50 @@ import datetime
 import pytz
 import tzlocal
 
-#
-# getSun
-# coords: { 'lat': lat_string, 'lng' : long_string }
-# returns { 'sunrise': datetime1, 'sunset': datetime2 } for given coords.
-# Datetimes are 'aware' and UTC.
-#
-def getSun(coord) :
-    response = urllib2.urlopen('http://api.sunrise-sunset.org/json?lat=' + coord['lat'] + '&lng=' + coord['lng'] + '&formatted=0')
-    html = response.read()
-    j = json.loads(html)
-    return { 'sunrise' :  pytz.utc.localize(datetime.datetime.strptime(j['results']['sunrise'], '%Y-%m-%dT%H:%M:%S+00:00')),
-             'sunset' :   pytz.utc.localize(datetime.datetime.strptime(j['results']['sunset'], '%Y-%m-%dT%H:%M:%S+00:00')) }
+
+class Coord :
+    def __init__(self, lat, lng):
+        self.lat, self.lng = lat, lng
+        
+
+class Place :
+    def __init__(self, coord) :
+        self.coord = coord
+        self.tz = tzlocal.get_localzone()
+
+    # findSunTimes
+    # sets internal sun times from external service
+    # times will be based on todays date
+    def findSunTimes(self) :
+        response = urllib2.urlopen('http://api.sunrise-sunset.org/json?lat=' +
+                                   self.coord.lat + '&lng=' + self.coord.lng + '&formatted=0')
+        html = response.read()
+        j = json.loads(html)
+        self.sunrise = pytz.utc.localize(datetime.datetime.strptime(j['results']['sunrise'], '%Y-%m-%dT%H:%M:%S+00:00'))
+        self.sunset = pytz.utc.localize(datetime.datetime.strptime(j['results']['sunset'], '%Y-%m-%dT%H:%M:%S+00:00'))
+        self.sunrise_local = self.sunrise.astimezone(self.tz)
+        self.sunset_local = self.sunset.astimezone(self.tz)
+
+    # isSunUp   
+    # Returns True if according to local system time, sun is presently up
+    #              at this Place. findSunTimes must have been called prior to this method.
+    def isSunUp(self) :
+        present = datetime.datetime.now(pytz.utc)
+        return( present > self.sunrise and present < self.sunset )
+
 
 def nkpCoord() :
-    return { 'lat' : '58.45', 'lng' : '17.04' }
+    return Coord('58.45', '17.04')
 
 def printNkpSun() :
-    sun = getSun( nkpCoord() )
-    tz = tzlocal.get_localzone()
-    sunrise = sun['sunrise'].astimezone(tz)
-    sunset = sun['sunset'].astimezone(tz)
-    print("Sunrise: " + sunrise.ctime())
-    print("Sunset: " + sunset.ctime())
+    nkp = Place(nkpCoord())
+    nkp.findSunTimes()
+    print("Sunrise: " + nkp.sunrise_local.ctime())
+    print("Sunset: " + nkp.sunset_local.ctime())
+
+
+def isSunUp() :
+    place = Place(nkpCoord())
+    place.findSunTimes()
+    return place.isSunUp()
+
